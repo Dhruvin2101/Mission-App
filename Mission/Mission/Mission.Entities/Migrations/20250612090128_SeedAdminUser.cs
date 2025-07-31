0c096b10f2654625bca9c989634cef1fe3e04104
dhruvincodes21 <dhruvin.codes@gmail.com>
2025-07-31 19:47:01 +0530

    inital commit

---
+++ Mission/Mission/Mission.Entities/Migrations/20250612090128_SeedAdminUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "password", "phone_number" },
+                values: new object[] { "admin@gmail.com", "Dharm", "admin", "8733006744" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "password", "phone_number" },
+                values: new object[] { "admin@tatvasoft.com", "Tatva", "Tatva@123", "9876543210" });
+        }
+    }
+}
---
+++ Mission/Mission/Mission.Entities/Migrations/20250613053502_The_Last_Mission.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class The_Last_Mission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "phone_number" },
+                values: new object[] { "admin@test.com", "Dhruvin", "9874563210" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "phone_number" },
+                values: new object[] { "admin@gmail.com", "Dharm", "8733006744" });
+        }
+    }
+}
